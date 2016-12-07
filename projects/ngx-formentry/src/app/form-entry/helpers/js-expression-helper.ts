bc3de40a67b5c190bf42c05e5b4ad23bc54710a9
vtuwei <vtuwei@ampath.or.ke>
2016-12-07 11:22:29 +0300

    OFO-18: Add helper functions for js expression

---
+++ projects/ngx-formentry/src/app/form-entry/helpers/js-expression-helper.ts
@@ -0,0 +1,86 @@
+
+export class JsExpressionHelper {
+
+  calcBMI(height, weight) {
+
+    let r;
+    if (height && weight) {
+      r = (weight / (height / 100 * height / 100)).toFixed(1);
+    }
+    return height && weight ? parseFloat(r) : null;
+  }
+
+  isEmpty(val) {
+
+    if (val === undefined || val === null || val === '' || val === 'null' || val === 'undefined') {
+      return true;
+    }
+
+    if (Array.isArray(val) && val.length === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  arrayContains(array, members) {
+
+    if (Array.isArray(members)) {
+
+      if (members.length === 0) {
+        return true;
+      }
+
+      let contains = true;
+
+      for ( let i = 0; i < members.length; i++ ) {
+        let val = members[i];
+        if (array.indexOf(val) === -1) {
+          contains = false;
+        }
+      };
+
+      return contains;
+    } else {
+      return array.indexOf(members) !== -1;
+    }
+  }
+
+  formatDate(value, format, offset) {
+
+    format = format || 'yyyy-MM-dd';
+    offset = offset || '+0300';
+
+    if (!(value instanceof Date)) {
+
+      value = new Date(value);
+      if (value === null || value === undefined) {
+      throw new Error('DateFormatException: value passed ' +
+      'is not a valid date');
+      }
+    }
+
+    return value; // TODO implement this
+    // return $filter('date')(value, format, offset);
+  }
+
+  arrayContainsAny(array, members) {
+
+    if (Array.isArray(members)) {
+      if (members.length === 0) {
+          return true;
+      }
+      let contains = false;
+
+      for ( let i = 0; i < members.length; i++ ) {
+
+        let val = members[i];
+        if (array.indexOf(val) !== -1) {
+            contains = true;
+        }
+      }
+      return contains;
+    } else {
+      return array.indexOf(members) !== -1;
+    }
+  }
+}
