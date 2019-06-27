ce7b0ec074c71b4747652378028778c78433ab55
madvincy <eugenekandie@gmail.com>
2019-06-27 15:56:00 +0300

    file uploader and file viewer fix

--- projects/ngx-formentry/src/components/file-upload/file-upload.module.ts
+++ projects/ngx-formentry/src/components/file-upload/file-upload.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-import { NgxFileUploaderModule } from 'ngx-file-uploader-ampath';
+import { NgxFileUploaderModule } from 'ngx-file-uploader';
+
 import { SharedModule } from '../../shared.module';
 // import { SelectModule } from 'ng2-select/ng2-select';
 import { SelectModule } from '../../components/select/select.module';
-import { RemoteFileUploadComponent } from './file-upload.component';
+import { FileUploadComponent } from './file-upload.component';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
-    imports: [CommonModule, SelectModule, FormsModule, NgxFileUploaderModule, SharedModule],
-    exports: [RemoteFileUploadComponent],
-    declarations: [RemoteFileUploadComponent],
+    imports: [CommonModule, SelectModule, FormsModule, SharedModule, WebcamModule, NgxFileUploaderModule],
+    exports: [FileUploadComponent],
+    declarations: [FileUploadComponent],
     providers: [],
 })
 export class RemoteFileUploadModule { }
--- projects/ngx-formentry/src/encounter-viewer/display-controls/file-preview.component.ts
+++ projects/ngx-formentry/src/encounter-viewer/display-controls/file-preview.component.ts
@@ -10,10 +10,13 @@ const noop = () => {};
 @Component({
     selector: 'file-preview',
     styles: [``],
-    template: `<div *ngIf="innerValue">
-              <img class="img-responsive"
-                [src]="innerValue | secure:this._dataSource.fetchFile" alt="image" />
-                </div>`,
+    template: `<div *ngIf="fileUuid">
+              <img *ngIf="!resultsIsPdf" class="img-responsive"
+                [src]="fileUuid | secure:this._dataSource.fetchFile" alt="image" />
+                </div>
+                <a *ngIf="resultsIsPdf" (click)="getUrl()" style="cursor: pointer"><u>Open PDF</u></a>
+                `,
+
     providers: [
       {
         provide: NG_VALUE_ACCESSOR,
@@ -22,9 +25,11 @@ const noop = () => {};
       }
     ]
   })
-export class FilePreviewComponent implements ControlValueAccessor {
+export class FilePreviewComponent implements ControlValueAccessor, OnInit {
     @Input() public source: any;
-    public innerValue = null;
+    public fileUuid = null;
+    public pdfUrl: any;
+    public resultsIsPdf = false;
     public _dataSource: DataSource;
     @Input()
     public get dataSource(): DataSource {
@@ -38,24 +43,32 @@ export class FilePreviewComponent implements ControlValueAccessor {
     private onTouchedCallback: () => void = noop;
     private onChangeCallback: (_: any) => void = noop;
     constructor(private encounterService: EncounterViewerService) {}
+    ngOnInit() {  }
     // get accessor
     get value(): any {
-      return this.innerValue;
+      return this.fileUuid;
     }
 
     // set accessor including call the onchange callback
     set value(v: any) {
-      if (v !== this.innerValue) {
-        this.innerValue = v;
+      if (v !== this.fileUuid) {
+        this.fileUuid = v;
       }
     }
     // Current time string.
 
     public writeValue(v: any) {
-      if (v !== this.innerValue) {
-          this.innerValue = v;
+      if (v !== this.fileUuid) {
+          this.fileUuid = v;
+         this.checkFileType;
       }
     }
+    public checkFileType() {
+      const re = /pdf/gi;
+          if (this.fileUuid.search(re) !== -1) {
+            this.resultsIsPdf = true;
+          }
+  }
 
     // From ControlValueAccessor interface
     public registerOnChange(fn: any) {
@@ -88,4 +101,9 @@ export class FilePreviewComponent implements ControlValueAccessor {
 
       // fileReader.readAsDataURL(fileToLoad);
     }
+  public getUrl() {
+      this.dataSource.fetchFile(this.fileUuid, 'pdf').subscribe((file) => {
+        window.open(file.changingThisBreaksApplicationSecurity , '_blank');
+      });
   }
+}
