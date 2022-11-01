87fee3e77c34d570bbf67a010bec62223055fef3
Ian <52504170+ibacher@users.noreply.github.com>
2022-11-01 13:36:10 -0400

    Various fixes to the radio buttons (#16)

--- projects/ngx-formentry/src/form-entry/question-models/interfaces/radio-button-options.ts
+++ projects/ngx-formentry/src/form-entry/question-models/interfaces/radio-button-options.ts
@@ -1,5 +1,6 @@
 import { BaseOptions } from '../interfaces/base-options';
 
 export interface RadioButtonOptions extends BaseOptions {
+  allowUnselect?: boolean;
   options: { key: string; value: string }[];
 }
