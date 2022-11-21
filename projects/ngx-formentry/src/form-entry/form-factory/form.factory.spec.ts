d22cdcd4e697bee80d3e4eecbcc7dd9cc6cadf28
Jose Francisco <josecarlos.francisco@emea.merkleinc.com>
2022-11-21 12:38:52 +0000

    fix lint for form factory tests

--- projects/ngx-formentry/src/form-entry/form-factory/form.factory.spec.ts
+++ projects/ngx-formentry/src/form-entry/form-factory/form.factory.spec.ts
@@ -281,7 +281,7 @@ describe('Form Factory:', () => {
     expect((thirdLevel.children['key1'] as LeafNode).control).toBeTruthy();
     expect(
       (thirdLevel.children['key1'] as LeafNode).control instanceof
-        AfeFormControl
+      AfeFormControl
     ).toBe(true);
     const control = (thirdLevel.children['key1'] as LeafNode)
       .control as AfeFormControl;
@@ -301,9 +301,9 @@ describe('Form Factory:', () => {
       questions: [],
       extras: {
         questionOptions: {
-        min: 0,
-        max: 10,
-      }
+          min: 0,
+          max: 10
+        }
       },
     });
 
