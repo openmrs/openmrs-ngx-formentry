b00307fb574b6ac84bbdc07dea506a16fb9ff940
icrc-loliveira <68058940+icrc-loliveira@users.noreply.github.com>
2023-03-14 10:28:31 +0000

    Update projects/ngx-formentry/src/form-entry/question-models/interfaces/diagnosis-question-options.ts
    Co-authored-by: Ian <52504170+ibacher@users.noreply.github.com>

--- projects/ngx-formentry/src/form-entry/question-models/interfaces/diagnosis-question-options.ts
+++ projects/ngx-formentry/src/form-entry/question-models/interfaces/diagnosis-question-options.ts
@@ -1,7 +1,7 @@
 import { BaseOptions } from '../interfaces/base-options';
 import {DataSource} from "./data-source";
 export interface DiagnosisQuestionOptions extends BaseOptions {
-  dataSource?: DataSource;
+  dataSource?: string;
   rendering: string;
   rank: 1 | 2;
   options?: Array<Record<string, string>>;
