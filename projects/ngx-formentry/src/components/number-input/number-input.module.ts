50864f98ff4999745c3d972b010b3f8701c882c0
Donald Kibet <chelashawdonald@yahoo.com>
2023-03-23 14:42:40 +0300

    enhancement: Add NumberInput directive and UI enhancement (#38)

--- projects/ngx-formentry/src/components/number-input/number-input.module.ts
+++ projects/ngx-formentry/src/components/number-input/number-input.module.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { NumberInputComponent } from './number-input.component';
+import { NumberInputDirective } from './number.directive';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
   exports: [NumberInputComponent],
-  declarations: [NumberInputComponent],
+  declarations: [NumberInputComponent,NumberInputDirective],
   providers: []
 })
 export class NumberInputModule {}
