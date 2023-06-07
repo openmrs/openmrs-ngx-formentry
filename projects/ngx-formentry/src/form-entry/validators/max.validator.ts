8f3aed6c95c133e3b235effe2538e4161f299cf5
Vineet Sharma <sharmava05@gmail.com>
2023-06-07 15:57:47 +0530

    Fixed the min and max validator for number inputs to validate value 0

--- projects/ngx-formentry/src/form-entry/validators/max.validator.ts
+++ projects/ngx-formentry/src/form-entry/validators/max.validator.ts
@@ -3,11 +3,18 @@ import { AfeFormControl } from '../../abstract-controls-extension/afe-form-contr
 export class MaxValidator {
   validate(max: number) {
     return (control: AfeFormControl): { [key: string]: any } => {
+      console.log('maxValue', max, control.value, typeof control.value);
+
       if (control.hidden) {
         return null;
       }
 
-      if (control.value && control.value.length !== 0) {
+      // Case 1: control.value is a number
+      // all the numbers should be passed for validation
+      // Case 2: control.value is not a number:
+      // if its empty string or null or undefined it will return false else it will pass through for validation
+      if (typeof control.value === 'number' || control.value) {
+        console.log('in');
         const v: number = control.value;
         return v <= max
           ? null
