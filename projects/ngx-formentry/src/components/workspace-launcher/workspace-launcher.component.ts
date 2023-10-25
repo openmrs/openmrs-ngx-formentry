ab25a6f2c607ad5960a2c0677a809d3521b93470
Brandon Istenes <bistenes@gmail.com>
2023-10-25 14:02:37 -0400

    More protection against version mismatch of esm-patient-common-lib

--- projects/ngx-formentry/src/components/workspace-launcher/workspace-launcher.component.ts
+++ projects/ngx-formentry/src/components/workspace-launcher/workspace-launcher.component.ts
@@ -13,6 +13,13 @@ export class WorkspaceLauncherComponent {
       console.error(
         "@openmrs/esm-patient-common-lib is not accessible. The 'workspace-launcher' question type can only be used in the context of the O3 patient chart, where the workspace is."
       );
+    } else if (
+      typeof window['_openmrs_esm_patient_common_lib']
+        .launchPatientWorkspace !== 'function'
+    ) {
+      console.error(
+        '@openmrs/esm-patient-common-lib is accessible, but the `launchPatientWorkspace` function is missing. It is likely that the version of @openmrs/esm-patient-common-lib that is being used is not compatible with this version of ngx-formentry.'
+      );
     } else {
       window['_openmrs_esm_patient_common_lib'].launchPatientWorkspace(
         'add-drug-order'
