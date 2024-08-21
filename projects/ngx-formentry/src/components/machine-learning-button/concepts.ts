6854977c05b8e1ddcd3e5bb404bec4c148e498e4
Patrick Waweru <97954453+PatrickWaweru@users.noreply.github.com>
2024-08-21 20:44:40 +0300

    More fixes due to change in HTS Eligibility screening form concepts V2 (#151)
    * More fixes due to change in HTS Eligibility screening form concepts V2
    
    * More fixes due to change in HTS Eligibility screening form concepts V3

---
+++ projects/ngx-formentry/src/components/machine-learning-button/concepts.ts
@@ -0,0 +1,8 @@
+const concepts = {
+    YES: 1065,
+    NO: 1066,
+    NPNonHospitalPatient: "06f16792-9611-40d4-82ec-9615930cc486",
+    AlcoholicSexAlways: "0d2f24cd-0bd9-4159-b71d-a92f2c11a396",
+};
+  
+export default concepts;
