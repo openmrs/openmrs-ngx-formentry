5a40ed3d78d7b66282f927007cdba010d58e6405
Donald Kibet <chelashawdonald@yahoo.com>
2025-02-26 10:46:17 +0300

    (feat) : Add helper function to retrieve obs value

--- projects/ngx-formentry/src/form-entry/helpers/js-expression-helper.ts
+++ projects/ngx-formentry/src/form-entry/helpers/js-expression-helper.ts
@@ -391,6 +391,14 @@ export class JsExpressionHelper {
     return '';
   }
 
+  getObsValue(
+    obs: Array<{ concept: { uuid: string }; value: any }>,
+    conceptUuid: string
+  ) {
+    const obsValue = obs.find((o) => o.concept.uuid === conceptUuid)?.value;
+    return obsValue;
+  }
+
   get helperFunctions() {
     const helper = this;
     return {
@@ -407,7 +415,8 @@ export class JsExpressionHelper {
       doesNotMatchExpression: helper.doesNotMatchExpression,
       calcGravida: helper.calcGravida,
       fetchData: helper.fetchData,
-      calculateZNutritionScore: helper.calculateZNutritionScore
+      calculateZNutritionScore: helper.calculateZNutritionScore,
+      getObsValue: helper.getObsValue
     };
   }
 }
